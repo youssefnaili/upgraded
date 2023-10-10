2394a5c32bac4feae8b49261a9b21c7e6010065d
youssefnaili <nailiyoussef01@gmail.com>
2023-10-10 10:56:16 +0100

    project

---
+++ js.js
@@ -0,0 +1,12 @@
+// function sendMail(params) {
+//     var m={
+//         from_name : document.getElementById("fullName"),
+//         email_id : document.getElementById("email"),
+//         message : document.getElementById("message"),
+
+//     }
+    
+// }
+function btn(){
+    return alert("you are connected with our collection")
+}
